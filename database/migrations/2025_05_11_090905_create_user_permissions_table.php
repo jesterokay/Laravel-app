bff2af7f1a39407fb5eca3fa86a9344d4e42f8cb
r-jester <kingofdyroth007@gmail.com>
2025-06-27 18:22:54 +0700

    Remove permission_id from user_permissions table

--- database/migrations/2025_05_11_090905_create_user_permissions_table.php
+++ database/migrations/2025_05_11_090905_create_user_permissions_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,10 +10,8 @@ return new class extends Migration
         Schema::create('user_permissions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('role_id');
-            $table->unsignedBigInteger('permission_id');
             $table->timestamps();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
         });
     }
 
